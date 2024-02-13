88fec86544c1a985a840b27349df0c2e94e752fa
Mario Aguirre <maguirre@spindlemedia.com>
2024-02-13 10:57:48 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DDNSUpdater
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
